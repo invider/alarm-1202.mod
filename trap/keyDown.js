b884144dddf2e25f68d3195f7c6985440fa76675
Igor Khotin <khotin@gmx.com>
2020-03-29 13:53:55 +0300

    [+] numpad support

--- trap/keyDown.js
+++ trap/keyDown.js
@@ -1,5 +1,10 @@
 function keyDown(e) {
-    let digit = e.which - 48
+    let digit
+    if (e.which >= 96) {
+        digit = e.which - 96
+    } else {
+        digit = e.which - 48
+    }
     if (digit >= 0 && digit < 10) {
         lab.space.panel.input(digit)
     }
---
+++ trap/numpadEnterDown.js
@@ -0,0 +1,3 @@
+function numpadEnterDown(e) {
+    trap.echo('spaceDown', e)
+}
